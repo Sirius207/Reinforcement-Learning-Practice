66bdd1e68f4db21ddff007919315606354baf221
Po Chun, Lu <oceanus11034@gmail.com>
2017-10-27 21:09:16 +0800

    [Add] bee practice

--- gym/alien.py
+++ gym/alien.py
@@ -1,14 +1,11 @@
 import gym
 from RL_brain import DeepQNetwork
 
-env = gym.make('CartPole-v0')
+env = gym.make('AirRaid-v0')
 env = env.unwrapped
 
 print(env.action_space)
 print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
-
 
 
 RL = DeepQNetwork(n_actions=env.action_space.n,
@@ -17,7 +14,7 @@ RL = DeepQNetwork(n_actions=env.action_space.n,
                   replace_target_iter=100, memory_size=2000,
                   e_greedy_increment=0.0008,)
 
-total_steps = 0 # log steps counts
+total_steps = 0
 
 for i_episode in range(100):
 
@@ -29,17 +26,8 @@ for i_episode in range(100):
 
         action = RL.choose_action(observation)
 
-        observation_, reward, done, info = env.step(action) # get next state
+        observation_, reward, done, info = env.step(env.action_space.sample())
 
-        # x: horizontal displacement , the more x, smaller r1
-        # theta: pole degree, the more theta, smaller r2
-
-        x, x_dot, theta, theta_dot = observation_
-        r1 = (env.x_threshold - abs(x))/env.x_threshold - 0.8
-        r2 = (env.theta_threshold_radians - abs(theta))/env.theta_threshold_radians - 0.5
-        reward = r1 + r2
-
-        # save memory
         RL.store_transition(observation, action, reward, observation_)
 
         if total_steps > 1000:
@@ -54,5 +42,5 @@ for i_episode in range(100):
 
         observation = observation_
         total_steps += 1
-# plot cost curve
+
 RL.plot_cost()
